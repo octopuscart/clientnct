ad88ee4750d8797bed051e2470b5ef41889e2c77
dev1 <dev@octopuscart.com>
2018-10-02 08:58:37 +0530

    changes

--- application/views/Account/login.php
+++ application/views/Account/login.php
@@ -38,13 +38,27 @@ $this->load->view('layout/header');
                 <div class="login-registration-field">
                     <h2 class="cart-area-title">Login</h2>
                     <form method="post" action="#">
-                        <label>Email address *</label>
-                        <input type="email" name="email" placeholder="Email " required=""/>
-                        <label>Password *</label>
-                        <input type="password" name="password" placeholder="Password *" required=""/>
-                        <label class="check">Lost your password?</label>
-                        <button class="btn-send-message disabled" name="signIn" type="submit" value="signIn">Login</button>
-                        <!--<span><input type="checkbox" name="remember"/>Remember Me</span>-->
+                        <ul>
+                            <li class="m_bottom_15 m_xs_bottom_15">
+                                <label for="input_1" class="d_inline_m d_sm_block w_sm_auto m_sm_bottom_5">Email address *:</label>
+                                <input type="email" name="email" id="input_1" placeholder="Email *" class="r_corners d_inline_m w_sm_full">
+                            </li>
+                            <li class="m_bottom_15 m_xs_bottom_15">
+                                <label for="input_1" class="d_inline_m d_sm_block w_sm_auto m_sm_bottom_5">Password *:</label>
+                                <input type="password" name="password" placeholder="Password *" id="input_1" class="r_corners d_inline_m w_sm_full">
+                            </li>
+                            <li class="m_bottom_15 m_xs_bottom_15">    
+                                <button name="signIn" type="submit" value="signIn" class="button_type_3 r_corners tt_uppercase fs_medium color_green tr_all f_left m_right_10 m_md_bottom_10">Login</button>
+                            </li>
+                        </ul>
+
+                        <!--                        <label>Email address *</label>
+                                                <input type="text" name="" id="input_1" class="r_corners d_inline_m w_sm_full">
+                                                <label>Password *</label>
+                                                <input type="password"  class="r_corners d_inline_m w_sm_full" name="password" placeholder="Password *" required=""/>
+                                                <label class="check">Lost your password?</label>
+                                                <button class="btn-send-message disabled" name="signIn" type="submit" value="signIn">Login</button>-->
+                                                <!--<span><input type="checkbox" name="remember"/>Remember Me</span>-->
                     </form>
                 </div>
             </div>
@@ -52,20 +66,45 @@ $this->load->view('layout/header');
                 <div class="login-registration-field">
                     <h2 class="cart-area-title">Register</h2>
                     <form>
-                        <label>First Name *</label>
-                        <input type="text" name="first_name" placeholder="First Name *">
-                        <label>Last Name *</label>
-                        <input type="text" name="last_name" placeholder="Last Name *">
-                        <label>Email address *</label>
-                        <input type="email" name="email" placeholder="Email *">
-                        <label>Password *</label>
-                        <input type="password" class="pass" name="password" placeholder="Password">
-                         <label>Confirm Password *</label>
-                        <input type="password" class="con_pass" name="con_password" placeholder="Confirm Password">
+                        <ul>
+                            <li class="m_bottom_15 m_xs_bottom_15">
+                                <label for="input_1" class="d_inline_m d_sm_block w_sm_auto m_sm_bottom_5">First Name *:</label>
+                                <input type="text" name="first_name" id="input_1" placeholder="First Name *" class="r_corners d_inline_m w_sm_full">
+                            </li>
+                            <li class="m_bottom_15 m_xs_bottom_15">
+                                <label for="input_1" class="d_inline_m d_sm_block w_sm_auto m_sm_bottom_5">Last Name *:</label>
+                                <input type="text" name="last_name" placeholder="Last Name *" id="input_1" class="r_corners d_inline_m w_sm_full">
+                            </li>
+                            <li class="m_bottom_15 m_xs_bottom_15">
+                                <label for="input_1" class="d_inline_m d_sm_block w_sm_auto m_sm_bottom_5">Email *:</label>
+                                <input type="email" name="email" id="input_1" placeholder="Email *" class="r_corners d_inline_m w_sm_full">
+                            </li>
+                            <li class="m_bottom_15 m_xs_bottom_15">
+                                <label for="input_1" class="d_inline_m d_sm_block w_sm_auto m_sm_bottom_5">Password *:</label>
+                                <input type="password" name="password" placeholder="Password *" id="input_1" class="r_corners d_inline_m w_sm_full">
+                            </li>
+                            <li class="m_bottom_15 m_xs_bottom_15">
+                                <label for="input_1" class="d_inline_m d_sm_block w_sm_auto m_sm_bottom_5">Confirm Password *:</label>
+                                <input type="password" name="con_password" placeholder="Confirm Password *" id="input_1" class="con_pass r_corners d_inline_m w_sm_full">
+                            </li>
+                            <li class="m_bottom_15 m_xs_bottom_15">    
+                                <button type="submit" value="Login" class="button_type_3 r_corners tt_uppercase fs_medium color_green tr_all f_left m_right_10 m_md_bottom_10">Register</button>
+                            </li>
+                        </ul>
+                        <!--                        <label>First Name *</label>
+                                                <input type="text" name="first_name" placeholder="First Name *">
+                                                <label>Last Name *</label>
+                                                <input type="text" name="last_name" placeholder="Last Name *">
+                                                <label>Email address *</label>
+                                                <input type="email" name="email" placeholder="Email *">
+                                                <label>Password *</label>
+                                                <input type="password" class="pass" name="password" placeholder="Password">
+                                                <label>Confirm Password *</label>
+                                                <input type="password" class="con_pass" name="con_password" placeholder="Confirm Password">-->
 
 
 
-                        <button class="btn-send-message disabled" type="submit" value="Login">Register</button>
+<!--                        <button class="btn-send-message disabled" type="submit" value="Login">Register</button>-->
                     </form>
                 </div>
             </div>
