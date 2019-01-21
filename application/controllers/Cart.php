0bbadbffaf6c252fe93f96cb45c84eb482e5465c
dev1 <dev@octopuscart.com>
2019-01-21 15:56:33 +0530

    changes

--- application/controllers/Cart.php
+++ application/controllers/Cart.php
@@ -252,7 +252,7 @@ class Cart extends CI_Controller {
 
                 $this->db->insert('user_order', $order_array);
                 $last_id = $this->db->insert_id();
-                $orderno = "RT" . date('Y/m/d') . "/" . $last_id;
+                $orderno = "NC" . date('Y/m/d') . "/" . $last_id;
                 $orderkey = md5($orderno);
                 $this->db->set('order_no', $orderno);
                 $this->db->set('order_key', $orderkey);
